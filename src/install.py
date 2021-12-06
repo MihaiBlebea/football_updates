a76e7c9b01bcd4b672001eea2d8713360f52c632
Mihai Blebea <mihaiserban.blebea@gmail.com>
2021-12-06 20:57:57 +0000

    removes print

--- src/install.py
+++ src/install.py
@@ -16,7 +16,6 @@ def main(cron):
 
 def install(cron):
 	path = pathlib.Path(__file__).parent.resolve()
-	print(path)
 	job = cron.new(
 		command=f"{path}/../virtualenv/bin/python3 {path}/poll.py >> {path}/../cron.log 2>&1",
 		comment=COMMENT
